e1aebe06d129e96edc80ded38cfae99d07916266
dondek65 <44544520+dondek65@users.noreply.github.com>
2018-10-28 16:27:48 +0100

    Update problem001.js

--- problem001.js
+++ problem001.js
@@ -1,14 +1,23 @@
 function triangleArea(a, b, c) 
 		{
-		if (a+b<=c || a+c<=b || b+c<=a || a<=0 || b<=0 || c<=0) 
+		if (a+b<=c || a+c<=b || b+c<=a) 
 		{
 			return -1;
 		} 
 		else 
 		{
+			if((a>0 && b>0 && c>0))
+			{
 			var obwod = a+b+c;
-			var area = Math.sqrt(p*(p-a)*(p-b)*(p-c)); //Dominik Luber
-			area = Math.round(area * 100) / 100;
-		return area;
+			var p = obwod/2;
+			var pole= Math.sqrt(p*(p-a)*(p-b)*(p-c));
+			pole = Math.round(pole * 100) / 100;
+			
+			return pole;
+			}
+			else
+			{
+				return -1;
+			}
 		}
 }
